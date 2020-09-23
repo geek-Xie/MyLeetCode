9ad0cd92f87e31d95e8acac3a31361f5eb644bc1
geek-Xie <504428538@qq.com>
2020-09-23 13:06:31 +0800

    2020.9.23题目更新

--- 01SumOfTwoNumer(Easy)/src/Solution.java
+++ 01SumOfTwoNumer(Easy)/src/Solution.java
@@ -1,3 +1,15 @@
+/**
+ * 给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ * 示例:
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,9 +23,9 @@ public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
 
-        for (int i = 0; i < nums.length - 1;i++){
-            for (int j = i + 1;j < nums.length;j++){
-                if (nums[i] + nums[j] == target){
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
                     res[0] = i;
                     res[1] = j;
                 }
@@ -32,18 +44,18 @@ public class Solution {
     public int[] twoSum2(int[] nums, int target) {
         int[] res = new int[2];
         // 创建一个哈希表来存储，注意此时的哈希表key存储的是数组中的数组，value是该数字的下标
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         // 对数组进行遍历
-        for (int i = 0;i < nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             // 查看哈希表中是否有满足能与当前位置的数字组合成目标值的数字，这里利用的就是哈希表查找快的特点
             int rest = target - nums[i];
             // 如果存在符合条件的值，就直接将下标保存到结果数组中返回
-            if (map.containsKey(rest)){
+            if (map.containsKey(rest)) {
                 res[0] = i;
                 res[1] = map.get(rest);
             }
             // 如果当前的哈希表中没有符合条件的数字，则将当前的数字和下标添加到哈希表中，并且继续遍历数组
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
         return res;
     }
