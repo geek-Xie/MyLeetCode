705c80f9ef6a00603b3c72b3f6fea121069fea44
xiexunchao <504428538@qq.com>
2020-12-19 12:06:35 +0800

    2020.12.19题目更新

---
+++ 160GetIntersectionNode(Easy)/src/Solution.java
@@ -0,0 +1,50 @@
+/**
+ * 160. 相交链表
+ *
+ * 编写一个程序，找到两个单链表相交的起始节点。
+ *
+ * 示例 1：
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Reference of the node with value = 8
+ * 输入解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ *
+ *
+ * 示例2：
+ * 输入：intersectVal= 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Reference of the node with value = 2
+ * 输入解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ *
+ *
+ * 示例3：
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 解释：这两个链表不相交，因此返回 null。
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    /**
+     * 使用双指针的方法，A和B指针分别往前遍历，当两个指针走到对应链表的末尾的时候，重定位到另一链表的头节点继续遍历，当两个
+     * 链表有重合节点的时候，两个指针相遇的节点就是该重合节点
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode ha = headA, hb = headB;
+        while (ha != hb) {
+            ha = ha != null ? ha.next : headB;
+            hb = hb != null ? hb.next : headA;
+        }
+        return ha;
+    }
+}
