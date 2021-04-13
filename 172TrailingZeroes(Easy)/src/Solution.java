8ae5a3d0a93be326eba6a283fa21fb7282b28d0e
xiexunchao <504428538@qq.com>
2021-04-13 11:04:01 +0800

    2021.4.13题目更新

---
+++ 172TrailingZeroes(Easy)/src/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * 172. 阶乘后的零
+ *
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ *
+ * 示例 1:
+ *
+ * 输入: 3
+ * 输出: 0
+ * 解释:3! = 6, 尾数中没有零。
+ *
+ * 示例2:
+ * 输入: 5
+ * 输出: 1
+ * 解释:5! = 120, 尾数中有 1 个零.
+ *
+ */
+
+public class Solution {
+    public int trailingZeroes(int n) {
+        int zeroCount = 0;
+        // We need to use long because currentMultiple can potentially become
+        // larger than an int.
+        long currentMultiple = 5;
+        while (n >= currentMultiple) {
+            zeroCount += (n / currentMultiple);
+            currentMultiple *= 5;
+        }
+        return zeroCount;
+    }
+}
